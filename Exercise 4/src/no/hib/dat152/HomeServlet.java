76a3cb8019b1a42540ff9be1a812f05e4bab6620
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-23 13:49:27 +0200

    Commit

--- Exercise 4/src/no/hib/dat152/HomeServlet.java
+++ Exercise 4/src/no/hib/dat152/HomeServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Home
  */
 @WebServlet("/home")
-public class Home extends HttpServlet {
+public class HomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Home() {
+    public HomeServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
