eb5f6db2082ab784f43a1a07cb6b8dc7067ff6aa
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-23 13:47:17 +0200

    I live to serve your servlets

--- Exercise 4/src/no/hib/dat152/CartServlet.java
+++ Exercise 4/src/no/hib/dat152/CartServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Home
+ * Servlet implementation class CartServlet
  */
-@WebServlet("/home")
-public class Home extends HttpServlet {
+@WebServlet("/cart")
+public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Home() {
+    public CartServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,13 +26,14 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("WEB-INF/jsp/Home.jsp").forward(request, response);;	
+		request.getRequestDispatcher("WEB-INF/jsp/Cart.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
