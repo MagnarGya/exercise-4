063a8db8ce1917cc176663ca3225fa30d1132071
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-23 14:26:34 +0200

    Added descriptions to the fake database

--- Exercise 4/src/no/hib/dat152/FakeDAO.java
+++ Exercise 4/src/no/hib/dat152/FakeDAO.java
@@ -2,8 +2,22 @@ package no.hib.dat152;
 
 public class FakeDAO {
 	Product[] products = {new Product(0,"Name1",2.50f,"imagepath1"),new Product(1,"Name2",3.50f,"imagepath2")};
+	String[][] descriptions = {{"This is description", "Dette er beskrivelse", "Donde esta la biblioteca?"}
+							  ,{"This is a different description", "Beskrivelse?", "Quanta costa la biblioteca?"}};
 	
 	Product getProduct(int pno) {
 		return products[pno];
+	}
+	
+	String getDescription(int pno, String language) {
+		int langcode = 0;
+		
+		if (language.equals("no")) {
+			langcode = 1;
+		} else if (language.equals("es")){
+			langcode = 2;
+		}
+		
+		return descriptions[pno][langcode];
 	}
 }
