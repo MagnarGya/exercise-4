e522286ee8e62df351c0fcce9ee47208201aef54
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-23 13:57:47 +0200

    Did a thing with the thing

---
+++ Exercise 4/src/no/hib/dat152/FakeDAO.java
@@ -0,0 +1,11 @@
+package no.hib.dat152;
+
+public class FakeDAO {
+	Product[] products = {new Product(0,"Name1",2.50f,"imagepath1"),new Product(1,"Name2",3.50f,"imagepath2")};
+	
+	Product getProduct(int pno) {
+		return products[pno];
+	}
+	
+	
+}
--- Exercise 4/src/no/hib/dat152/Product.java
+++ Exercise 4/src/no/hib/dat152/Product.java
@@ -1,8 +1,33 @@
 package no.hib.dat152;
 
 public class Product {
-	int number;
+	int pno;
+
 	String pName;
 	float priceInEuro;
 	String imageFile;
+	
+	Product(int pno, String pName, float priceInEuro, String imageFile) {
+		this.pno = pno;
+		this.pName = pName;
+		this.priceInEuro = priceInEuro;
+		this.imageFile = imageFile;
+	}
+	
+	public int getPno() {
+		return pno;
+	}
+
+	public String getpName() {
+		return pName;
+	}
+
+
+	public float getPriceInEuro() {
+		return priceInEuro;
+	}
+
+	public String getImageFile() {
+		return imageFile;
+	}
 }
