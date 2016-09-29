1d3c2bdae79c3ddf8cb1bbcb4306deb218db32cb
Martin <Martin@MartinsFury>
2016-09-29 12:31:16 +0200

    corrijo la español

--- Exercise 4/src/no/hib/dat152/FakeDAO.java
+++ Exercise 4/src/no/hib/dat152/FakeDAO.java
@@ -2,8 +2,8 @@ package no.hib.dat152;
 
 public class FakeDAO {
 	Product[] products = {new Product(0,"White Chair",2.50f,"WEB-INF/white-chair.jpg"),new Product(1,"Black Chair",3.50f,"WEB-INF/black-chair.jpg")};
-	String[][] descriptions = {{"This chair is the ultimate chair for your living room", "Denne stolen er den ultimate stuestolen", "Donde esta la biblioteca?"}
-							  ,{"This chair is the best chair for your sleeping room", "Denne stolen er den beste stolen for soverommet", "Quanta costa la biblioteca?"}};
+	String[][] descriptions = {{"This chair is the ultimate chair for your living room", "Denne stolen er den ultimate stuestolen", "La último silla de sala"}
+							  ,{"This chair is the best chair for your sleeping room", "Denne stolen er den beste stolen for soverommet", "La silla mejor para el dormitorio"}};
 	
 	Product getProduct(String pn) {
 		int pno = Integer.parseInt(pn);
