d70a834e727e9205bc04c22623fab31bf52313c6
Magnar Gya <magnar.gya@hotmail.com>
2016-09-29 14:25:07 +0200

    I wish

--- Exercise 4/src/no/hib/dat152/FakeDAO.java
+++ Exercise 4/src/no/hib/dat152/FakeDAO.java
@@ -1,7 +1,7 @@
 package no.hib.dat152;
 
 public class FakeDAO {
-	static final Product[] products = {new Product(0,"White Chair",2.50f,"WEB-INF/white-chair.jpg"),new Product(1,"Black Chair",3.50f,"WEB-INF/black-chair.jpg")};
+	static final Product[] products = {new Product(0,"White Chair",2.50f,"white-chair.jpg"),new Product(1,"Black Chair",3.50f,"black-chair.jpg")};
 	static final String[][] descriptions = {{"This chair is the ultimate chair for your living room", "Denne stolen er den ultimate stuestolen", "La último silla de sala"}
 							  ,{"This chair is the best chair for your sleeping room", "Denne stolen er den beste stolen for soverommet", "La silla mejor para el dormitorio"}};
 	
