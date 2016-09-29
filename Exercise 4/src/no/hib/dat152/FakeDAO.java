5d8ff327ea76b467aaa7fd3c366cb6c7310f174c
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-29 13:32:56 +0200

    Static

--- Exercise 4/src/no/hib/dat152/FakeDAO.java
+++ Exercise 4/src/no/hib/dat152/FakeDAO.java
@@ -1,16 +1,16 @@
 package no.hib.dat152;
 
 public class FakeDAO {
-	Product[] products = {new Product(0,"White Chair",2.50f,"WEB-INF/white-chair.jpg"),new Product(1,"Black Chair",3.50f,"WEB-INF/black-chair.jpg")};
-	String[][] descriptions = {{"This chair is the ultimate chair for your living room", "Denne stolen er den ultimate stuestolen", "La último silla de sala"}
+	static final Product[] products = {new Product(0,"White Chair",2.50f,"WEB-INF/white-chair.jpg"),new Product(1,"Black Chair",3.50f,"WEB-INF/black-chair.jpg")};
+	static final String[][] descriptions = {{"This chair is the ultimate chair for your living room", "Denne stolen er den ultimate stuestolen", "La último silla de sala"}
 							  ,{"This chair is the best chair for your sleeping room", "Denne stolen er den beste stolen for soverommet", "La silla mejor para el dormitorio"}};
 	
-	Product getProduct(String pn) {
+	 static Product getProduct(String pn) {
 		int pno = Integer.parseInt(pn);
 		return products[pno];
 	}
 	
-	String getDescription(int pno, String language) {
+	static String getDescription(int pno, String language) {
 		int langcode = 0;
 		
 		if (language.equals("no")) {
